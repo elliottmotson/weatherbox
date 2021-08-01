5df397f6a950ffc40642d2d08245b94982b908ff
elliottmotson <78193700+elliottmotson@users.noreply.github.com>
2021-08-01 21:45:15 +0100

    networking init

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+import socket
+import os
+import fnmatch
+
+
+def main():
+
+    print('Initialising setup')
+
+    if haskey = false:
+
+        for f_name in os.listdir('./'):
+            if f_name.endswith('license.txt'):
+                print("Licence file exists.")
+                license = open("./license.txt" , "a")
+                licensekey = license.read()
+                print("LICENSE KEY - " + license)
+                haskey = true
+            else
+                print("ERROR 0001: NO LICENCE FILE")
+
+    else:
+        keyvalidate()
+
+
+def keyinput():
+
+    print('Enter activation key: ')
+    licensekey = input()
+    license = open("./license.txt","w")
+    license.write(licensekey)
+
+def keyvalidate():
+
+    netsetup()
+    socket.send(licensekey.encode())
+    serverreply = socket.recv(1024)
+
+
+def netsetup():
+    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+    PORT = 64206        # Port to listen on (non-privileged ports are > 1023)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        conn, addr = s.accept()
+        with conn:
+            print("Connected to: ", addr)
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
