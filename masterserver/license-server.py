4e0e8d2ccbd1c636b10e28653fe6a304b243f722
elliottmotson <78193700+elliottmotson@users.noreply.github.com>
2021-08-02 18:33:02 +0100

    License-Client connection established with sockets

--- masterserver/license-server.py
+++ masterserver/license-server.py
@@ -7,12 +7,14 @@ import time
 validated = False
 data = ''
 
+
 def main():
 
     print('Initialising license server setup')
     launchremote()
     clientkey()
     time.sleep(10000)
+
 
 def launchremote():
 
@@ -28,45 +30,15 @@ def launchremote():
         print("CONNECTION TO POSSIBLE CLIENT ESTABLISHED")
         print("Verifying...")
         clientsocket.send(bytes("4321", "utf-8"))
-
-
-
-def netsetup():
-    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-    PORT = 4206        # Port to listen on (non-privileged ports are > 1023)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((HOST, PORT))
-        s.listen()
-        print('SOCKET LISTENING ON PORT: ' , PORT)
-        conn, addr = s.accept()
-        with conn:
-            print("Request from ", addr)
-            while true:
-                data = conn.recv(1024)
-                validated = keyvalidate(data.decode())
-<<<<<<< Updated upstream
-                    if validated = true
-                    conn.send("4321")
-                else:
-                if not data:
-                    print("Net setup failed to receive incoming data")
-                    break
-=======
-                if validated == True:
-                    conn.sendall("4321")
-        if not data:
-            print("Net setup failed to receive incoming data")
-
->>>>>>> Stashed changes
+        print("Sent activation reply to client ", address, ": 4321")
 
 
 def clientkey():
-
     print('Validating client ' + ' with key: ' + data)
     licensekey = input()
-    license = open("./license.txt","w")
+    license = open("./license.txt", "w")
     license.write(licensekey)
+
 
 def keyvalidate(data):
 
--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ import fnmatch
 import time
 import sys
 
+
 def main():
 
     print('Initialising box setup')
 
-
-    netsetup()
+    connectremote()
     print("NETWORKING COMPLETE - GREAT SUCCESS!")
     time.sleep(10000)
 
@@ -19,8 +19,9 @@ def keyinput():
 
     print('Enter activation key: ')
     licensekey = input()
-    license = open("./license.txt","w")
+    license = open("./license.txt", "w")
     license.write(licensekey)
+
 
 def connectremote():
 
@@ -28,32 +29,10 @@ def connectremote():
     PORT = 4206
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((HOST, PORT))
-    incoming = s.recv(1024)
-    print("Connected master server: ", HOST)
-    print("MESSAGE FROM LICENSING SERVER: " + incoming.decode("utf-8"))
+    while True:
+        incoming = s.recv(1024)
+        print(incoming.decode("utf-8"))
 
 
-<<<<<<< Updated upstream
-def netsetup():
-    HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-    PORT = 64206        # Port to listen on (non-privileged ports are > 1023)
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT))
-        s.listen()
-        conn, addr = s.accept()
-        with conn:
-            print("Connected master server: ", addr)
-            while true:
-                print("Sending license key")
-                conn.send("1234")
-                data = conn.recv(1024)
-                if data.decode() = "4321"
-                    print("SERVER REPLY SUCCESSFUL. LICENSING COMPLETE")
-                    return true
-                if not data:
-                    return false
-
-=======
->>>>>>> Stashed changes
 if __name__ == '__main__':
     main()
